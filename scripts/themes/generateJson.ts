80b3d94ec483dc882f0eda3da4c81273eb00f69a
eating-coleslaw <10554267+eating-coleslaw@users.noreply.github.com>
2018-04-21 21:38:57 -0500

    mostly working, maybe?

---
+++ scripts/themes/generateJson.ts
@@ -0,0 +1,21 @@
+/**
+ * This file is only a script file that should only be executed by the npm scripts.
+ */
+import { createThemeFile } from './../../src/themes/index';
+import * as path from 'path';
+import * as fs from 'fs';
+import * as painter from '../helpers/painter';
+
+createThemeFile().then((fileName: string) => {
+    const filePath = path.resolve('src', fileName);
+    const out = path.resolve('out', 'src', fileName);
+    fs.rename(filePath, out, (err) => {
+        if (err) {
+            console.error(painter.red(err.stack));
+        } else {
+            console.log('> Nebula Color Theme:', painter.green(`Successfully generated color theme JSON file!`));
+        }
+    });
+}).catch(err => {
+    console.error(painter.red(err));
+});
--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 'use strict';
 
 import * as vscode from 'vscode';
-import * as i18n from './i18n';
+import * as i18n from './i18N';
 import * as commands from './commands';
 import { showStartMessages } from './messages/start';
 import { detectConfigChanges } from './helpers/change-detection';
